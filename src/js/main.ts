b27d5a3ecaeaf8aa1795e9bcb8b0dee99905ea53
Boskolife <boskoboskoboskobosko@gmail.com>
2024-01-20 23:55:27 +0200

    add new project

---
+++ src/js/main.ts
@@ -0,0 +1,10 @@
+// core version + navigation, pagination modules:
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import { findHref } from './findHref';
+
+
+findHref();
