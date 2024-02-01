484371cc922da969eb47a331f0217b5507bc1f1a
Boskolife <boskoboskoboskobosko@gmail.com>
2024-02-01 13:36:30 +0200

    add slide active element

--- src/js/swiper.ts
+++ src/js/swiper.ts
@@ -6,18 +6,34 @@ import { Navigation, Pagination } from 'swiper/modules';
 
 export const swiper = new Swiper('.works_slider', {
   slidesPerView: 1,
-  centeredSlides:true,
+  centeredSlides: true,
   modules: [Navigation, Pagination],
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+  on: {
+    slideChange: function () {
+      // const swiper = this as Swiper;
+      const slides = document.querySelectorAll('.swiper-slide');
+      const step = document.querySelectorAll('.step');
+      
+      step.forEach((bullet: Element, index: number) => {
+        (bullet as HTMLElement).classList.remove('active_step');
+      });
+      
+      // Добавление класса активности к текущему активному слайду и его навигационному элементу
+      const activeIndex = swiper.realIndex;
+      (slides[activeIndex] as HTMLElement).classList.add('active');
+      (step[activeIndex] as HTMLElement).classList.add('active_step');
+    },
   },
 });
 
 export const swiperReviews = new Swiper('.reviews_swiper', {
   slidesPerView: 2,
   modules: [Navigation, Pagination],
-  spaceBetween:50,
+  spaceBetween: 50,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
@@ -25,8 +41,8 @@ export const swiperReviews = new Swiper('.reviews_swiper', {
   pagination: {
     el: '.swiper-pagination',
     renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (index + 1) + '</span>';
-      }
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
   },
   breakpoints: {
     320: {
@@ -34,6 +50,6 @@ export const swiperReviews = new Swiper('.reviews_swiper', {
     },
     1024: {
       slidesPerView: 2,
-    }
-  }
+    },
+  },
 });
